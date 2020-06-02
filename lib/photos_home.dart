7971c622d3cfbf5a868fc091777a9fabec47853c
javeedishaq <ishaqjaveed1@gmail.com>
2020-06-02 11:44:27 +0500

    wallpaper list shown

--- lib/photos_home.dart
+++ lib/photos_home.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_wallpaper_app/models/image_model.dart';
+import 'package:http/http.dart' as http;
 
 class PhotosHome extends StatefulWidget {
   @override
@@ -7,24 +11,66 @@ class PhotosHome extends StatefulWidget {
 }
 
 class _PhotosHomeState extends State<PhotosHome> {
+  List<Photo> photos = [];
+
+  Future<List<Photo>> _getData() async {
+    final response = await http.get(
+        'https://api.pexels.com/v1/search/?page=1&per_page=24&query=nature',
+        headers: {
+          "Authorization":
+              "563492ad6f91700001000001bc3309718f724de0aa6ae3871ab37403"
+        });
+
+    if (response.statusCode == 200) {
+      var parsed = jsonDecode(response.body);
+
+      photos =
+          (parsed['photos'] as List).map((img) => Photo.fromJson(img)).toList();
+    }
+    print(photos);
+    return photos;
+  }
+
+  @override
+  void initState() {
+    _getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StaggeredGridView.countBuilder(
-        crossAxisCount: 4,
-        itemCount: 8,
-        itemBuilder: (BuildContext context, int index) => new Container(
-            color: Colors.green,
-            child: new Center(
-              child: new CircleAvatar(
-                backgroundColor: Colors.white,
-                child: new Text('$index'),
+      backgroundColor: Colors.teal[100],
+      appBar: AppBar(
+        title: Text("Wallpapers"),
+      ),
+      body: FutureBuilder(
+        future: _getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return StaggeredGridView.countBuilder(
+              crossAxisCount: 4,
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) => Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      '${snapshot.data[index].src.tiny}',
+                    ),
+                  ),
+                ),
               ),
-            )),
-        staggeredTileBuilder: (int index) =>
-            new StaggeredTile.count(2, index.isEven ? 2 : 1),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
+              staggeredTileBuilder: (int index) =>
+                  StaggeredTile.count(2, index.isEven ? 2 : 1),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
